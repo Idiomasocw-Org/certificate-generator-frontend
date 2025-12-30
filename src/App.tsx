833ede31c941a99442f74d62b0ba5f7d95dbb8a7
barbara arias <barbaraarias844@gmail.com>
2025-12-30 15:41:48 -0500

    feat: setup project with Vite, React, Tailwind CSS. Closes #1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-6 text-gray-800">Sistema Generador de Certificados</h1>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded shadow-lg transition duration-300">
+                    Botón de Prueba Tailwind
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default App
